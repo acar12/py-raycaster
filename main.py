10f7f7e29c5e143e2fe98e1e6bb32d541a76be8a
Ayush S. <ayush.sharma.se@gmail.com>
2023-09-02 12:04:56 -0500

    rendering

--- main.py
+++ main.py
@@ -1,49 +1,108 @@
 import math
 import pygame
 import grid
+from player import Player
 pygame.init()
 
+# constants
 width, height = 1300, 900
 fps = 60
 fov = math.radians(60)
-wall_height = 0.5
+wall_height = 1
 dist_to_projection = (width / 2) / math.tan(fov / 2)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Raycaster")
 
+wall = pygame.image.load("textures/wall.png")
+
 # game set-up
 grid = grid.Raycaster([
-    [1,1,1,1,1],
-    [1,0,0,0,1],
-    [1,0,0,0,1],
-    [1,0,0,0,1],
-    [1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ])
+max_dist = math.sqrt(len(grid.grid) ** 2 + len(grid.grid[0]) ** 2)
+player = Player(
+    2, 2, 180,
+    0.1, 0.1, 
+    grid
+)
 
 def draw():
-    for i in range(0, width, 5):
-        angle = i / width * fov
-        col, dist, tile = grid.raycast(2, 2, angle)
-        if tile > 0:
-            if dist != 0:
-                dist *= math.cos(angle)
-                draw_height = wall_height / dist * dist_to_projection
-                print(draw_height)
-                top = (height - draw_height) // 2
-                pygame.draw.rect(screen, (255, 255, 255), (i, top, 5, draw_height))
+    buff = []
+    slice_width = 1
+    for i in range(0, width, slice_width):
+        rel_ang = (i / width - 0.5) * fov
+        angle = rel_ang + player.rot
+        raycast = grid.raycast(*player.pos, angle)
+        if raycast:
+            col, dist, tile = raycast
+            if tile > 0:
+                if dist != 0:
+                    dist *= math.cos(rel_ang)
+                    dist = abs(dist)
+
+                    draw_height = wall_height / dist * dist_to_projection
+                    top = (height - draw_height) // 2
+                    c = 80 + 175 * ((max_dist - dist) / max_dist) ** 3
+                    pygame.draw.rect(screen, (c,c,c), (i, top, slice_width, draw_height))
+                    pygame.draw.rect(screen, (0, 0, 0), (i, top + draw_height, slice_width, height - top - draw_height))
+                    # slice = pygame.transform.scale(wall, (320, draw_height))
+                    # if col[0] % 1 != 0:
+                    #     tx = col[0] % 1 * 320
+                    #     screen.blit(slice, (i, top), (tx, 0, slice_width, draw_height))
+                    # else:
+                    #     tx = col[1] % 1 * 320
+                    #     screen.blit(slice, (i, top), (tx, 0, slice_width, draw_height))
+            buff.append(col)
+
+    # mini-map
+    num_rows = len(grid.grid)
+    num_cols = len(grid.grid[0])
+    cell_size = 15
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, num_rows * cell_size, num_cols * cell_size))
+    for i in range(len(grid.grid)):
+        for j in range(len(grid.grid[0])):
+            if grid.grid[j][i] > 0:
+                pygame.draw.rect(screen, (255, 0, 0), 
+                    (i * cell_size, j * cell_size, cell_size, cell_size))
+                
+    pygame.draw.circle(screen, (0, 0, 255), (player.pos[0] * cell_size, player.pos[1] * cell_size), 5)
+    for c in buff:
+        pygame.draw.line(screen, (0, 255, 0), 
+            (player.pos[0] * cell_size, player.pos[1] * cell_size),
+            (c[0] * cell_size, c[1] * cell_size))
 
 
 
 timer = pygame.time.Clock()
 running = True
+locked = True
 while running:
     # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            locked = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            locked = True
+    # custom input processing
+    # pygame.event.set_grab(locked)
+    player.process_key_input(pygame.key.get_pressed())
+    # player.process_mouse_input(*pygame.mouse.get_rel())
     # rendering
+    screen.fill((0, 0, 0))
     draw()
     pygame.display.update()
+    
     # end
     timer.tick(fps)
